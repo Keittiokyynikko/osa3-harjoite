975922f6d4e75a07a8fdd9f3ae4a6c974e2c15fc
Keittiokyynikko <karvinen.juuso@gmail.com>
2021-06-18 12:35:16 +0300

    front

---
+++ osa3/build/precache-manifest.256d02b20c7664b9791477fa2d51a3ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c60a7bd5bc7255f3b3775fdf1664cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75a3c3bf9c46497b6ff6",
+    "url": "/static/css/main.367c6821.chunk.css"
+  },
+  {
+    "revision": "3b3451b0804334a834ba",
+    "url": "/static/js/2.2bfb4239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bfb4239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a3c3bf9c46497b6ff6",
+    "url": "/static/js/main.c3ed55cc.chunk.js"
+  },
+  {
+    "revision": "bf9adf24cd1b969a9b57",
+    "url": "/static/js/runtime-main.20d2aa3e.js"
+  }
+]);
--- osa3/index.js
+++ osa3/index.js
@@ -1,89 +1,111 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
 const cors = require('cors')
+const { response } = require('express')
 app.use(cors())
+
+app.use(express.static('build'))
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
-
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-
-  return maxId + 1
+const unknownEndpoint = (req, res) => {
+  response.status(404).send({error: 'unknown endpoint'})
 }
 
-app.post('/api/notes', (request, response) => {
+app.use(unknownEndpoint)
 
-  const note = request.body
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-  if(!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
-  const newNote = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId()
-  }
-  
-  notes = notes.concat(newNote)
+  next(error)
+}
 
-  response.json(newNote)
-})
+app.use(errorHandler)
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if(note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  })
 
-  if(note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
+
+/*const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+
+  return maxId + 1
+} */
+
+app.post('/api/notes', (request, response) => {
+
+  const body = request.body
+
+  if(!body.content === undefined) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+  
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.put('api/notes/:id', (request, response, next) => {
+  const body = req.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(updateNote)
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.enc.PORT || 3001
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
